671f36648c9b22e8c373c4d2d96920ec6cb47d6b
Franco Di Rosa <fdr.2013@hotmail.com>
2020-10-25 12:15:44 -0300

    Exercise 1 solved

---
+++ script.js
@@ -0,0 +1,32 @@
+// Ejercicio N°1: Variables y Operadores
+
+/*
+a) Crear dos variables numéricas y utilizar el operador
+suma para guardar el valor de la suma de ambos números
+en una 3er variable.
+*/
+
+var num1 = 1;
+var num2 = 2;
+var sum = num1 + num2;
+
+/*
+b) Crear dos variables de tipo String y concatenarlas guardando
+el resultado en una 3er variable.
+*/
+
+var string1 = "Hello";
+var string2 = "World";
+var string3 = string1 + string2;
+
+/*
+c) Crear dos variables de tipo String y sumar el largo de cada
+variable (cantidad de letras del string) guardando el resultado
+la suma una 3er variable (utilizar length).
+*/
+
+var string1 = "Hello";
+var string2 = "World";
+var qchar = string1.length + string2.length;
+
+
