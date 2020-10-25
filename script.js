62d30df5ceb6b6935c3fe97888700db81d6d53f8
Franco Di Rosa <fdr.2013@hotmail.com>
2020-10-25 20:45:40 -0300

    Exercise 5 solved

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ myArray2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Ago
 var myArrayCopy = myArray2.slice(4,11);
 console.log(myArrayCopy);
 
-// If Else
+// Ejercicio N°4: If Else
 
 /*
 a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(),
@@ -193,3 +193,51 @@ if (myAge < 2){
 } else {
     alert("Anciano");
 }
+
+// Ejercicio N°5: For
+
+/*
+a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle
+for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+*/
+
+var myArray = ["pen" , "pencil" , "eraser" , "ruler" , "square"];
+for (var i = 0; i < myArray.length; i++){
+    alert(myArray[i]);
+}
+
+/*
+b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar
+una alerta por cada palabra modificada.
+*/
+
+for (var i = 0; i < myArray.length; i++){
+    myArray[i] = myArray[i].substring(0,1).toUpperCase() + myArray[i].substring(1);
+    alert(myArray[i]);
+}
+
+/*
+c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del
+punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la
+variable sentence. Al final mostrar una única alerta con la cadena completa.
+*/
+
+var myArray = ["pen" , "pencil" , "eraser" , "ruler" , "square"];
+var sentence  = [];
+for (var i = 0; i < myArray.length; i++){
+    sentence.push(myArray[i]);
+}
+alert(sentence);
+
+/*
+d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el
+número de la repetición, es decir que al final de la ejecución del bucle for deberia
+haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por
+la consola del navegador el array final (utilizar console.log).
+*/
+
+var myArray3  = [];
+for (var i = 0; i < 10; i++){
+    myArray3.push(i);
+}
+console.log(myArray3);
